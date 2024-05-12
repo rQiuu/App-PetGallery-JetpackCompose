e0c6df1b537d6e01f86eefa65aa4eb8fd84c4c26
rQiuu <rizqiuu01@gmail.com>
2024-05-12 22:32:37 +0700

    tambahan comment pada main activity

--- app/src/main/java/com/tugas_fe/myapp/petGallery/MainActivity.kt
+++ app/src/main/java/com/tugas_fe/myapp/petGallery/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
+                //run app
                     PetApp()
             }
         }
